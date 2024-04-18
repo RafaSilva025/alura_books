f878c603b2fc15454a4660d07a62b0899c0bec6c
Rafa <rafasilvasimoes25@gmail.com>
2024-04-18 16:55:42 -0300

    [Terminando o style components do curso]

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -2,25 +2,21 @@ import Logo from '../Logo'
 import OpcoesHeader from '../OpcoesHeader'
 import IconesHeader from '../IconesHeader'
 import styled from 'styled-components'
-
-
+ 
 const HeaderContainer = styled.header`
-    .App-header {
-    background-color: #FFF;
-    display: flex;
-    justify-content: center;
-  }
+   background-color: #FFF;
+   display: flex;
+   justify-content: center;
 `
-
-
+ 
 function Header() {
-    return (
-      <HeaderContainer>
-        <Logo/>
-        <OpcoesHeader/>
-        <IconesHeader></IconesHeader>
-      </HeaderContainer>
-    )
+   return (
+       <HeaderContainer>
+           <Logo/>
+           <OpcoesHeader/>
+           <IconesHeader/>
+       </HeaderContainer>
+   )
 }
-
+ 
 export default Header
--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,27 +1,24 @@
-import styled from 'styled-components'
 import logo from '../../imagens/logo.svg'
+import styled from 'styled-components'
 
 const LogoContainer = styled.div`
-     display: flex;
+    display: flex;
     font-size: 30px;
 `
 const LogoImage = styled.img`
     margin-right: 10px;
-
 `
 
-function Logo() {
-
-    return (
+function Logo () {
+    return(
         <LogoContainer>
-            <LogoImage 
-                src={logo} 
-                alt='logo' 
-                className='logo-img'
-            ></LogoImage>
+            <LogoImage
+                src={logo}
+                alt='logo'
+            />
             <p><strong>Alura</strong>Books</p>
         </LogoContainer>
     )
-}        
+}
 
 export default Logo
