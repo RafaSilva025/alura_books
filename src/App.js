2155d5f0470095da0eed62081ad01b4617b4eb73
Rafa <rafasilvasimoes25@gmail.com>
2024-04-18 11:54:42 -0300

    [UPD]- criando os components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
-import Logo from './componentes/Logo'
-import perfil from './imagens/perfil.svg'
-import sacola from './imagens/sacola.svg'
-import OpcoesHeader from './componentes/OpcoesHeader';
-
-const icones = [perfil, sacola]
+import Header from './componentes/Header';
 
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <Logo></Logo>
-        <OpcoesHeader></OpcoesHeader>
-      </header>
+      <Header />
     </div>
   );
 }
