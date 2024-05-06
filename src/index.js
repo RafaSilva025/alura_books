87eb98c7845768e7ca622af8341090ada3b58398
Rafa <rafasilvasimoes25@gmail.com>
2024-05-06 08:34:22 -0300

    [UPD] - Primeira Rota com React Router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createGlobalStyle } from 'styled-components'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -29,10 +30,16 @@ const GlobalStyle = createGlobalStyle`
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+      <GlobalStyle />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/favoritos" element={<p>Oi!</p>} />
+              <Route path="/" element={<App />} />
+          </Routes>
+      </BrowserRouter>
+      <App />
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
