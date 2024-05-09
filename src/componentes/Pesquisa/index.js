c9405c0fd52309c23fe11742e870ab6e76ada839
Rafa <rafasilvasimoes25@gmail.com>
2024-05-09 08:59:12 -0300

    [UPD- inserir e deletar livros]

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,7 +1,9 @@
 import Input from '../Input'
 import styled from 'styled-components'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { getLivros } from '../../servicos/livros'
+import { useEffect } from 'react'
+import { postFavorito } from '../../servicos/favoritos'
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -47,15 +49,20 @@ const Resultado = styled.div`
 
 function Pesquisa() {
     const [livrosPesquisados, setLivrosPesquisados] = useState([])
-    const [ livros, setLivros ] = useState([])
+    const [livros, setLivros] = useState([])
 
     useEffect(() => {
         fetchLivros()
     }, [])
 
     async function fetchLivros() {
-        const livrosDaApi = getLivros()
-        setLivros(livrosDaApi) 
+        const livrosDaAPI = await getLivros()
+        setLivros(livrosDaAPI)
+    }
+
+    async function insereFavorito(id) {
+        await postFavorito(id)
+        alert(`O livro de id: ${id} foi inserido com sucesso`)
     }
 
     return (
@@ -71,7 +78,7 @@ function Pesquisa() {
                 }}
             />
             { livrosPesquisados.map( livro => (
-                <Resultado>
+                <Resultado onClick={() => insereFavorito(livro.id)}>
                     <img src={livro.src}/>
                     <p>{livro.nome}</p>
                 </Resultado>
